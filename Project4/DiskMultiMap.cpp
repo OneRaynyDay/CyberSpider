85996f2b5198fd54ef3ca2f9800102981b774fa1
OneRaynyDay <peifeng2005@gmail.com>
2016-03-10 21:41:50 -0800

    Finished this.

--- Project4/DiskMultiMap.cpp
+++ Project4/DiskMultiMap.cpp
@@ -42,9 +42,7 @@ bool DiskMultiMap::updateNode(Node node, int offset){
     return bf.write(node, offset);
 }
 /* =========================================== MAIN FUNCTIONS ========================================== */
-/* ===== SPEC : DISKMULTIMAP :: DISKMULTIMAP() & ~DISKMULTIMAP() =====
- You MUST implement a constructor and destructor for your DiskMultiMap. The constructor MUST initialize your object. When the destructor returns, the disk file associated with the hash table must have been closed (either previously by a client calling DiskMultiMap::close() or by the destructor itself).
-*/
+/* ===== SPEC : DISKMULTIMAP :: DISKMULTIMAP() & ~DISKMULTIMAP() ===== */
 
 DiskMultiMap::DiskMultiMap(){
 }
@@ -53,14 +51,7 @@ DiskMultiMap::~DiskMultiMap(){
     bf.close();
 }
 
-/* ===== SPEC : DISKMULTIMAP :: CREATENEW() =====
- You MUST implement the createNew() method. This method MUST create an empty, open hash table in a binary disk file with the specified filename, with the specified number of empty buckets. If there is already an existing disk file with the same filename at the time that the createNew() method is called, then your createNew() method MUST overwrite the original file with a brand-new file (our BinaryFile::createNew() method will automatically perform this overwriting for you). Once the hash table has been created, you may then proceed to add new associations, search through it, or delete associations with the object’s other methods.
- 
- If your DiskMultiMap object already has been used to open or create a disk-based hash table, and the createNew() method is called, your method MUST first close the currently- open data file, and then proceed with creating the newly specified data file.
- 
- If your method fails for any reason, it MUST return false. Otherwise, if it succeeds it MUST return true.
- This method MUST run in O(B) time where B is the number of buckets in the hash table.
-*/
+/* ===== SPEC : DISKMULTIMAP :: CREATENEW() ===== */
 bool DiskMultiMap::createNew(const string& filename, unsigned int numBuckets){
     /* already has been used to open or create a disk-based hash table,
      and the createNew() method is called,
@@ -103,11 +94,7 @@ bool DiskMultiMap::createNew(const string& filename, unsigned int numBuckets){
     return flag;
 }
 
-/* ===== SPEC : DISKMULTIMAP :: OPENEXISTING() =====
- You MUST implement the openExisting() method. This method MUST open a previously-created disk-based hash table with the specified filename. If the specified disk file does not exist on the hard drive, then your method MUST return false. If your method succeeds in opening the specified file, then it MUST return true. Once the hash table has been opened, you may then proceed to search through it, add new associations, or delete associations.
- If your DiskMultiMap object already has been used to open or create a disk-based hash table, and the openExisting() method is called, your method MUST first close the currently-open data file, and then proceed with opening the newly specified data file.
- This method MUST run in O(1) time.
-*/
+/* ===== SPEC : DISKMULTIMAP :: OPENEXISTING() ===== */
 bool DiskMultiMap::openExisting(const std::string& filename){
     /* already has been used to open or create a disk-based hash table,
      and the createNew() method is called,
@@ -122,38 +109,13 @@ bool DiskMultiMap::openExisting(const std::string& filename){
     return true;
 }
 
-/* ===== SPEC : DISKMULTIMAP :: CLOSE() =====
- You MUST implement the close() method. This method is used to ensure that all data written to your hash table is safely saved to the binary disk file, and the binary disk file is closed. If a client doesn't call this method, then your DiskMultiMap destructor MUST close the binary disk file. Obviously, the client should only call the close() method if they first called the createNew() or openExisting() methods to create/open a hash table data file. Otherwise, the close() method should do nothing, as it should is called on an already- closed file.
- This method MUST run in O(1) time.
-*/
+/* ===== SPEC : DISKMULTIMAP :: CLOSE() ===== */
 
 void DiskMultiMap::close(){
     bf.close();
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: INSERT() =====
- You MUST implement the insert() method. This method adds a new association to the disk-based hash table associating the specified key, e.g. “foo.exe”, with the associated value and context strings, e.g. “www.google.com” and “m12345”. Since you’re implementing a multimap, your insert function MUST be able to properly store duplicate associations. Consider this code:
- 
- int main() {
-     DiskMultiMap x;
-     bool insert(const std::string& key, const std::string& value,
-     const std::string& context)
- }
- 
- x.createNew("myhashtable.dat",100); // empty, with 100 buckets
- x.insert("hmm.exe", "pfft.exe", "m52902"); 
- x.insert("hmm.exe", "pfft.exe", "m52902");
- x.insert("hmm.exe", "pfft.exe", "m10001"); 
- x.insert("blah.exe", "bletch.exe", "m0003");
- 
- The above would result in your hash table holding the following associations (although not necessarily in the ordering shown below. We place no restrictions on what order your associations are stored in your hash table):
- “hmm.exe” {“pfft.exe”, “m52902”}, {“pfft.exe”, “m52902”}, {“pfft.exe”, “m10001”}
- “blah.exe” {“bletch.exe”, “m0003”}
-
- Remember, this method MUST insert the new association directly into your hash table’s disk file, NOT to an in-memory hash table! This means that every valid insert call will result in one or more modifications to your hash table’s on-disk data structures!
- 
- This method MUST run in O(N/B) time assuming your hash table holds a generally diverse set of N items and has B buckets. If you are inserting an association with a particular key X, and the hash table already holds K associations with key X, then this method may run in O(K) time.
-*/
+/* ===== SPEC : DISKMULTIMAP() :: INSERT() ===== */
 
 bool DiskMultiMap::insert(const std::string& key, const std::string& value, const std::string& context){
     // Now let's create a Node struct that holds all this information:
@@ -233,64 +195,7 @@ bool DiskMultiMap::insert(const std::string& key, const std::string& value, cons
         return false;
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: SEARCH() =====
- You MUST implement the search() method. This method is used to find all associations in the hash table that match the specified key string. Your method MUST return a DiskMultiMap::Iterator object, which is analogous to a C++ iterator. The user can then use this iterator to enumerate all associations that matched the specified key string. If no associations matched the specified key string, then the Iterator returned must be invalid (i.e., one for which DiskMultiMap::Iterator::isValid() returns false). Here’s how you might use the search() method – look for the bold parts:
- 
- int main() {
-     DiskMultiMap x;
-     x.createNew("myhashtable.dat",100); // empty, with 100 buckets
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m10001"); 
-     x.insert("blah.exe", "bletch.exe", "m0003");
-     DiskMultiMap::Iterator it = x.search("hmm.exe"); 
-     if (it.isValid())
-     {
-         cout << "I found at least 1 item with a key of hmm.exe\n"; 
-         do
-         {
-             MultiMapTuple m = *it; // get the association cout << "The key is: " << m.key << endl;
-             cout << "The value is: " << m.value << endl; cout << "The context is: " << m.context << endl; cout << endl;
-             ++it; // advance iterator to the next matching item 
-         } while (it.isValid());
-     }
- }
- 
- The above code might print:
- 
- I found at least 1 item with a key of hmm.exe 
- 
- The key was: hmm.exe
- The value was: pfft.exe
- The context was: m52902
- 
- The key was: hmm.exe
- The value was: pfft.exe
- The context was: m52902
- 
- The key was: hmm.exe
- The value was: pfft.exe 
- The context was: m00001
- 
- Here’s another example:
- int main() {
-     DiskMultiMap x;
-     x.createNew("myhashtable.dat",100); // empty, with 100 buckets
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m10001"); 
-     x.insert("blah.exe", "bletch.exe", "m0003");
-     DiskMultiMap::Iterator it = x.search("goober.exe"); 
-     if ( ! it.isValid())
-        cout << "I couldn't find goober.exe\n";
- }
- 
- The above code would print:
- I couldn’t find goober.exe
- 
- As you can see, your DiskMultiMap’s search() method (and the returned Iterator) works similarly to the C++ find() methods provided by the STL associative container classes!
- This method MUST run in O(N/B) time assuming your hash table holds a generally diverse set of N items and has B buckets. If you are searching for an item with a particular key X, and the hash table holds K associations with key X, then this method may run in O(K) time
-*/
+/* ===== SPEC : DISKMULTIMAP() :: SEARCH() ===== */
 
 DiskMultiMap::Iterator DiskMultiMap::search(const std::string& key){
     /* === CHECKING IF FILE IS OPEN FIRST! === */
@@ -306,28 +211,7 @@ DiskMultiMap::Iterator DiskMultiMap::search(const std::string& key){
     return Iterator(&bf, key, bucket.start);
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: ERASE() =====
- You MUST implement the erase() method. This method MUST remove all associations in the hash table that exactly match the specified key, value, and context strings passed in and return the number of associations removed. Your hash table MUST somehow track the location of all deleted nodes, so these nodes can be reused should one or more new associations later be added to the hash table after deletion. Here’s an example how your erase() method might be used:
- 
-
- int main() {
-     DiskMultiMap x;
-     x.createNew("myhashtable.dat",100); // empty, with 100 buckets
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m52902"); 
-     x.insert("hmm.exe", "pfft.exe", "m10001"); 
-     x.insert("blah.exe", "bletch.exe", "m0003");
-     // line 1
-     if (x.erase("hmm.exe", "pfft.exe", "m52902") == 2) 
-        cout << "Just erased 2 items from the table!\n";
-     // line 2
-     if (x.erase("hmm.exe", "pfft.exe", "m10001") > 0)
-        cout << "Just erased at least 1 item from the table!\n";
-     // line 3
-     if (x.erase("blah.exe", "bletch.exe", "m66666") == 0)
-        cout << "I didn't erase this item cause it wasn't there\n";
- }
-*/
+/* ===== SPEC : DISKMULTIMAP() :: ERASE() ===== */
 
 int DiskMultiMap::erase(const std::string& key, const std::string& value, const std::string& context){
     /* === CHECKING IF FILE IS OPEN FIRST! === */
@@ -402,13 +286,7 @@ int DiskMultiMap::erase(const std::string& key, const std::string& value, const 
     return counter;
 }
 
-/* =========================================================================================================================== */
-/* ========================================== DISKMULTIMAP :: ITERATOR IMPLEMENTATION ======================================== */
-/* =========================================================================================================================== */
-
-/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ITERATOR() =====
-The default constructor must create an Iterator in an invalid state. This constructor must run in O(1) time.
-*/
+/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ITERATOR() ===== */
 
 DiskMultiMap::Iterator::Iterator(){
     curAddress = NONE;
@@ -416,14 +294,7 @@ DiskMultiMap::Iterator::Iterator(){
     bf = nullptr;
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ITERATOR() =====
- You may write other Iterator constructors with whatever parameters you like. It is your
- choice whether the Iterator created by any such constructor is in a valid or invalid state.
- 
- If the Iterator created is in a valid state, then it must Any such constructor MUST run in
- O(N/B) time or better, assuming that is being initialized to point into a hash table holding 
- a generally diverse set of N items, with B buckets.
- */
+/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ITERATOR() ===== */
 
 DiskMultiMap::Iterator::Iterator(BinaryFile* binaryf, std::string k, int curAddr){
     bf = binaryf;
@@ -447,16 +318,10 @@ DiskMultiMap::Iterator::Iterator(BinaryFile* binaryf, std::string k, int curAddr
     //now, the value of curAddress is either at the first node of key or at NONE, and we're done.
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: isValid() =====
- This method MUST return true if the iterator is in a valid state, and false otherwise. This
- method MUST always run in O(1) time.
- */
+/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: isValid() ===== */
 bool DiskMultiMap::Iterator::isValid() const{ return (curAddress != NONE); }
 
-/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ++() =====
- You MUST implement a prefix increment operator (but not a decrement operator or a postfix increment operator) for the Iterator class. This operator MUST do nothing if the Iterator it’s called on is invalid. Otherwise, the ++ operator MUST advance the Iterator to the next association in the DiskMultiMap with the same key as the association the Iterator currently points to, if there is one; if there is no next association with the same key, then the ++ operator MUST change the Iterator’s state to invalid. The method returns a reference to the Iterator it's called on.
- This method MUST run in O(N/B) time or less, assuming that is called on an Iterator pointing into a hash table holding a generally diverse set of N items, with B total buckets.
- */
+/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: ++() ===== */
 DiskMultiMap::Iterator& DiskMultiMap::Iterator::operator++(){
     if(!isValid())
         return *this;
@@ -474,18 +339,7 @@ DiskMultiMap::Iterator& DiskMultiMap::Iterator::operator++(){
     return *this;
 }
 
-/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: *() =====
- You MUST implement the unary * operator for your Iterator class – this is the dereference operator, and it allows you to examine an association pointed to by a valid Iterator, using syntax akin to that of dereferencing a C++ pointer or an STL iterator. The * operator MUST return an object of type MultiMapTuple:
- struct MultiMapTuple {
-     std::string key;
-     std::string value;
-     std::string context;
- };
- 
- If the Iterator is in an invalid state, each string in the MultiMapTuple returned is the empty string. Otherwise, the key, value and context strings of the MultiMapTuple returned have values equal to the key, value, and context components of the association the Iterator points to.
- This method MUST run in O(1) time.
- Note: Reading data from a disk file is literally thousands or millions of times slower than reading data from your computer’s RAM, so consider the performance consequences of the following code:
- */
+/* ===== SPEC : DISKMULTIMAP() :: ITERATOR() :: *() ===== */
 MultiMapTuple DiskMultiMap::Iterator::operator*(){
     if(!isValid())
         return MultiMapTuple();
@@ -500,50 +354,3 @@ MultiMapTuple DiskMultiMap::Iterator::operator*(){
     tuple.context = node.context;
     return tuple;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Project4/IntelWeb.cpp
+++ Project4/IntelWeb.cpp
@@ -2,8 +2,8 @@
 //  IntelWeb.cpp
 //  Project4
 //
-//  Created by hongshuhong on 3/7/16.
-//  Copyright © 2016 hongshuhong. All rights reserved.
+//  Created by Ray Zhang on 3/7/16.
+//  Copyright © 2016 Ray Zhang. All rights reserved.
 //
 
 #include <stdio.h>
@@ -52,25 +52,7 @@ IntelWeb::~IntelWeb()
 {
     close();
 }
-/* ===== SPECS : INTELWEB :: CREATENEW() =====
- The createNew() method MUST create a set of empty disk-based data structures to hold
- information about all of the telemetry log data that you’ll be loading from one or more
- telemetry log data files. Your disk-based data structures MUST consist of one or more
- DiskMultiMaps, and you MUST not use any other disk files.
- 
- -- make searching through the data structures to discover attacks (in your crawl() method) as efficient as possible
- 
- -- The first parameter is a filename prefix
- 
- -- no restrictions on how many DiskMultiMaps your createNew() method creates, or what data the hash tables hold
- 
- -- The second parameter to createNew() specifies the expected number of telemetry data items that your data structures will have to hold (e.g., 2,000,000)
- 
- -- well-performing hash tables have a load factor of at most .75, so your hash tables MUST also have a planned load factor less than or equal to .75 given the value of the second parameter
- 
- -- The createNew() method MUST close any DiskMultiMaps this IntelWeb object might have created or opened before creating any new ones. If successful, createNew() MUST
- have overwritten any existing DiskMultiMap disk files with new, empty versions. This method MUST run in O(maxDataItems) time
- */
+/* ===== SPECS : INTELWEB :: CREATENEW() ===== */
 bool IntelWeb::createNew(const std::string& filePrefix, unsigned int maxDataItems)
 {
     //The createNew() method MUST close any DiskMultiMaps this IntelWeb object might have created or opened before creating any new ones.
@@ -87,29 +69,7 @@ bool IntelWeb::createNew(const std::string& filePrefix, unsigned int maxDataItem
     }
     return false;
 }
-/* ===== SPECS : INTELWEB :: OPENEXISTING() =====
- open an existing set of DiskMultiMap(s) that were previously created and populated using calls to IntelWeb's createNew() with the given file
- prefix and ingest().
- 
- Any data previously ingested into these DiskMultiMaps will be
- available for access after the openExisting() method has been called.
- 
- If this method is able to successfully open its data structures, then it MUST return true.
- 
- On the other hand, if there is any problem (e.g., one or more of your data files cannot be
- opened), this method MUST close any successfully-opened data files (so that all data
- files end up closed) and return false.
- 
- Once this method has been called successfully, the user may use the ingest() method to
- ingest one or more additional telemetry log data files and store their information within
- your data structures, the crawl() method to search for attacks, and the purge() method to
- purge specific telemetry data items from the data structures.
- 
- This method MUST close any DiskMultiMaps this IntelWeb object might have created or
- opened before opening any new ones.
- 
- This method MUST run in O(1) time.
- */
+/* ===== SPECS : INTELWEB :: OPENEXISTING() ===== */
 bool IntelWeb::openExisting(const std::string& filePrefix)
 {
     fromMap.close();
@@ -133,44 +93,7 @@ void IntelWeb::close()
     fromMap.close();
 }
 
-/* ===== SPECS : INTELWEB :: INGEST() =====
- The ingest() method is used to insert all the data from a telemetry log file of the specified
- name into your disk-based data structures. While you will ingest data from each
- telemetry file into your data structures once, you’ll be searching through these structures
- (using crawl()) over and over and over, so the speed of ingestion is much less important
- than the speed at which you can search your data structures. So you MUST design your
- data structures to facilitate fast searching rather than trying to make insertion fast if it will
- slow down searching.
- 
- ---------------------------
- File Creation Events
- 
- A File Creation Event indicates that a program (e.g., foo.exe) created another program (e.g., bar.exe) on a particular machine (e.g., m491). Its format is:
- 
- machineID <space> creatorFile <space> createdFile <newline>
- 
- m491 foo.exe bar.exe
- 
- ---------------------------
- File Download Events
- 
- A File Download Event indicates that a given URL (e.g., http://www.viruses.com/downloads) sent a program (e.g., foo.exe) down to a particular machine (e.g., m7711).
- 
- machineID <space> URL <space> downloadedFile <newline>
- 
- m7711 http://www.viruses.com/downloads foo.exe
- 
- ---------------------------
- File Contact Events
- A File Contact Event indicates that a given program (e.g., foo.exe) contacted a specific URL.
- The components of a line can be machine names, program names, or URLs, each of
- which is a string of non-whitespace characters. The lines are in one of three different
- formats:
-
- machineID <space> programFile <space> URL <newline>
- 
- m15006 foo.exe http://www.viruses.com/downloads
- */
+/* ===== SPECS : INTELWEB :: INGEST() ===== */
 bool IntelWeb::ingest(const std::string& telemetryFile){
     //refresh our prevMap
     prevMap.clear();
@@ -206,22 +129,7 @@ bool IntelWeb::ingest(const std::string& telemetryFile){
     return true;
 }
 
-/* ===== SPECS : INTELWEB :: CRAWL() =====
- responsible for (a) discovering and outputting an ordered
- vector of all malicious entities found in the previously-ingested telemetry, and (b)
- outputting an ordered vector of every interaction5 discovered that includes at least
- one malicious entity, by “crawling” through the ingested telemetry!
- 
- Your crawl() method MUST discover ALL malicious entities within the provided telemetry, 
- and it MUST place these discovered malicious entities in lexicographical order 
- (i.e., sorted as strings are sorted in increasing order) within the badEntitiesFound parameter.
- 
- If the same interaction occurs on two different machines, e.g.:
- {from=“www.yahoo.com”, to=“r.exe”, context=“m10101”} 
- {from=“www.yahoo.com”, to=“r.exe”, context=“m09090”}
- 
- 
- */
+/* ===== SPECS : INTELWEB :: CRAWL() ===== */
 
 int IntelWeb::processPairs(DiskMultiMap::Iterator& iter,
                            unordered_set<string>& badSet,
@@ -271,18 +179,9 @@ unsigned int IntelWeb::crawl(const std::vector<std::string>& indicators,
                    unsigned int minPrevalenceToBeGood,
                    std::vector<std::string>& badEntitiesFound,
                    std::vector<InteractionTuple>& badInteractions){
-    /*The badEntitiesFound parameter MUST NOT contain ANY values other than those malware entities discovered within the telemetry, even if the badEntitiesFound vector was not empty upon the call to crawl()
-     */
+
     badEntitiesFound.clear();
-    /* ===== INPUTS =====
-     1. An aggregated set of millions of lines of telemetry - you’ve already digested this telemetry and stored it in the disk-based data structures with the ingest() method!
-     2. A vector of one or more initially-known malicious entities (e.g., a vector of one or more strings with the names of bad files and/or websites) – these are passed to crawl() in the indicators parameter.
-     3. A prevalence threshold value Pgood (e.g., Pgood = 12) that can be used to determine whether an entity is too popular to be considered malicious: If the number of telemetry log lines that refer to entity E is greater than or equal to Pgood, then E is considered legitimate (aka not malicious) because it’s too popular to be an advanced attack – this prevalence threshold is the minPrevalenceToBeGood parameter. 
-     
-     While the number of ingested telemetry interactions can be so huge that we are requiring that you store them in disk files, you may assume that the number of malicious entities and bad interactions, while possibly large, is still small enough for them to fit in RAM
-     
-     You may also assume that the number of entities whose prevalence is at least minPrevalenceToBeGood is small enough for them to fit in RAM.
-     */
+
     unordered_set<string> badSet;
     unordered_map<string, bool> prevalents;
     
@@ -308,22 +207,6 @@ unsigned int IntelWeb::crawl(const std::vector<std::string>& indicators,
         processPairs(iter2, badSet, badInteractionSet, prevalents, minPrevalenceToBeGood, q, false);
     }
     
-    /* ===== GOTCHAS =====
-     Gotchas with the crawl() Method
-     Be careful when writing the crawl() method; it’s very easy to crawl in circles if you don’t
-     take precautions not to do so! For example, consider the following three telemetry items:
-     
-     m1 www.virus.com b.exe
-     m2 b.exe c.exe
-     m3 c.exe www.virus.com
-     */
-    
-    
-    /* ===== RETURN =====
-     Note: You should not return the number of interactions discovered, but the number of malicious entities!
-     */
-    
-    
     /* Sort in Lexicographical ordering! */
     badEntitiesFound = vector<string>(badSet.begin(), badSet.end());
     badInteractions = vector<InteractionTuple>(badInteractionSet.begin(), badInteractionSet.end());
@@ -332,41 +215,7 @@ unsigned int IntelWeb::crawl(const std::vector<std::string>& indicators,
     return badEntitiesFound.size();
 }
 
-/* ===== SPEC : INTELWEB :: PURGE() =====
- The purge command is used to remove ALL references to a specified entity (e.g., a
- filename or website) from your IntelWeb disk-based data structures. Subsequent crawls of
- your telemetry using the crawl() method MUST NOT find any references to the specified
- entry after it’s been purged.
- 
- For example, let’s assume that you previously ingested the following telemetry items into
- your IntelWeb log files:
- 
- Machine Initiator Entity Target Entity
- m1 a.exe b.exe 
- m1 a.exe b.exe
- m2 b.exe c.exe
- m3 a.exe d.exe
- m4 www.bad.com c.exe
- 
- Let’s further assume, for the purposes of this example, that your IntelWeb class uses a
- disk-based map data structure that associates each Initiator Entity to all related telemetry
- items that hold that Initiator Entity9:
- 
- a.exe {m1,a.exe,b.exe}, {m1,a.exe,b.exe}, {m3, a.exe, d.exe}
- b.exe {m2, b.exe, c.exe}
- www.bad.com {m4, www.bad.com, c.exe}
- 
- AFTER CALLING THIS:
- purge(b.exe);
- 
- Your resulting disk-based data structure should look like this after the purge() method
- finishes (struck-out items have been completely removed from the file):
- 
- a.exe {m3, a.exe, d.exe}
- b.exe
- www.bad.com {m4, www.bad.com, c.exe}
- 
- */
+/* ===== SPEC : INTELWEB :: PURGE() ===== */
 bool IntelWeb::purge(const std::string& entity){
     // erase requires parameters:
     // const std::string& key, const std::string& value, const std::string& context
@@ -396,6 +245,3 @@ bool IntelWeb::purge(const std::string& entity){
     }
     return i > 0;
 }
-
-
-
